b8f01e3ec96e0db0dc4442f66db74555cac82255
GeremiaGabriele04 <s326835@studenti.polito.it>
2026-04-13 10:05:03 +0200

    first half 13/4

--- countdown.py
+++ countdown.py
@@ -1,1 +1,13 @@
+#UTILE UTILIZZARE DEBUG PER VEDERE RECURSION
+def countdown_recursive(n):
+    #condizione terminale
+    if n==0:
+        print("Stop")
+    #condizione non terminale
+    else:
+        print(n)
+        countdown_recursive(n-1)
 
+if __name__ == "__main__":
+    N=4
+    countdown_recursive(N)
